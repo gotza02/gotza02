dd13531fd4272a4b91f19bc41cf69772e5b3a9fb
gotza02 <47317509+gotza02@users.noreply.github.com>
2022-08-03 18:20:13 +0700

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,8 +1,7 @@
 modpath="/data/adb/modules/GT_STABLE/"
-chmod +x "${modpath}"
-chmod +x "${modpath}*"
-chmod +x "${modpath}*/*"
-chmod +x "${modpath}*/*/*"
+chmod 777 "${modpath}"
+chmod 777 "${modpath}system"
+chmod 777 "${modpath}system/bin"
 # Wait to boot be completed
 until [[ "$(getprop sys.boot_completed)" -eq "1" ]] || [[ "$(getprop dev.bootcomplete)" -eq "1" ]]; do
 	sleep 5
@@ -16,9 +15,9 @@ wget -qO "${modpath}module.prop" "https://raw.githubusercontent.com/gotza02/gotz
 wget -qO "${modpath}system/bin/optimize" "https://raw.githubusercontent.com/gotza02/gotza02/main/OPTIMIZE.sh"
 wget -qO "${modpath}system/bin/gamimg" "https://raw.githubusercontent.com/gotza02/gotza02/main/gaming"
 wget -qO "${modpath}service.sh" "https://raw.githubusercontent.com/gotza02/gotza02/main/service.sh"
-chmod 7777 "${modpath}*"
-chmod 7777 "${modpath}system/*"
-chmod 7777 "${modpath}system/bin/*"
+chmod 777 "${modpath}"
+chmod 777 "${modpath}system"
+chmod 777 "${modpath}system/bin"
 sh "${modpath}system/bin/updategt"
 sh "${modpath}system/bin/trimcache"
 sh "${modpath}system/bin/tweakgt"
