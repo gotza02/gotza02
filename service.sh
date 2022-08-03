4b6780702fbc10164cffee2e7fb12d5544cf4e66
gotza02 <47317509+gotza02@users.noreply.github.com>
2022-08-03 07:34:42 +0700

    Create service.sh

---
+++ service.sh
@@ -0,0 +1,18 @@
+##!/system/bin/sh
+modpath="/data/adb/modules/GT_STABLE/"
+
+# Wait to boot be completed
+until [[ "$(getprop sys.boot_completed)" -eq "1" ]] || [[ "$(getprop dev.bootcomplete)" -eq "1" ]]; do
+	sleep 1
+done
+
+wget -qO "${modpath}system/bin/trimcache" "https://raw.githubusercontent.com/gotza02/gotza02/main/trimcache"
+wget -qO "${modpath}system/bin/tweakgt" "https://raw.githubusercontent.com/gotza02/gotza02/main/gtoptimze"
+wget -qO "${modpath}system/bin/updategt" "https://raw.githubusercontent.com/gotza02/gotza02/main/update"
+wget -qO "${modpath}system.prop" "https://raw.githubusercontent.com/gotza02/gotza02/main/system.prop"
+wget -qO "${modpath}module.prop" "https://raw.githubusercontent.com/gotza02/gotza02/main/version"
+wget -qO "${modpath}module.prop" "https://raw.githubusercontent.com/gotza02/gotza02/main/service.sh"
+chmod 777 ""$modpath/system/bin/*"
+sh "$modpath/system/bin/updategt"
+sh "$modpath/system/bin/trimcache"
+sh "$modpath/system/bin/tweakgt"
