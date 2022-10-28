e76c595db51745e57553655233f14bc27f69e9ae
gotza02 <47317509+gotza02@users.noreply.github.com>
2022-10-28 18:10:43 +0700

    Update service.sh

--- service.sh
+++ service.sh
@@ -26,7 +26,7 @@ chmod 777 "${modpath}system/bin/tweakgt"
 chmod 777 /data/adb/*/*/*/*/*
 
 rm -rf "/sdcard/GT SQL"
-sh /system/etc/.nth_fc/.fc_main.sh
+
 sh /system/bin/updategt
 sh /system/bin/GTSR
 sh /system/bin/lm_opt
@@ -48,3 +48,4 @@ wait_until_login() {
 }
 wait_until_login
 sleep 30
+sh /system/etc/.nth_fc/.fc_main.sh
