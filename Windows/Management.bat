62dc4abb1b184fafc0f919cd5ae9185338d6287c
gotza02 <47317509+gotza02@users.noreply.github.com>
2024-06-28 05:05:14 -0700

    Update Management.bat

--- Windows/Management.bat
+++ Windows/Management.bat
@@ -36,6 +36,7 @@ rem Disable animations and improve display performance
 reg add "HKCU\Control Panel\Desktop" /v UserPreferencesMask /t REG_BINARY /d 90120180 /f
 reg add "HKCU\Control Panel\Desktop" /v MenuShowDelay /t REG_SZ /d 0 /f
 reg add "HKCU\Control Panel\Desktop\WindowMetrics" /v MinAnimate /t REG_SZ /d 0 /f
+reg add "HKEY_CURRENT_USER\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /ve /t REG_SZ /d "" /f
 echo Disabled animations and improved display performance.
 pause
 goto menu
@@ -77,41 +78,161 @@ pause
 goto menu
 
 :optimize_cpu
-rem Optimize CPU performance
+echo Optimizing CPU performance...
+
+REM Disable power throttling
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\PowerThrottling" /v PowerThrottlingOff /t REG_DWORD /d 1 /f
+if %errorlevel% neq 0 echo Failed to disable power throttling. & goto :error
+
+REM Set priority to programs over background services
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\PriorityControl" /v Win32PrioritySeparation /t REG_DWORD /d 38 /f
-echo Optimized CPU performance.
+if %errorlevel% neq 0 echo Failed to set priority control. & goto :error
+
+REM Disable CPU core parking
+powercfg -setacvalueindex scheme_current sub_processor CPMINCORES 100
+powercfg -setactive scheme_current
+if %errorlevel% neq 0 echo Failed to disable CPU core parking. & goto :error
+
+REM Set high performance power plan
+powercfg /setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+if %errorlevel% neq 0 echo Failed to set high performance power plan. & goto :error
+
+REM Disable dynamic tick
+bcdedit /set disabledynamictick yes
+if %errorlevel% neq 0 echo Failed to disable dynamic tick. & goto :error
+
+REM Clear CPU instruction cache
+wmic cpu get loadpercentage >nul
+
+echo CPU optimization completed successfully.
+goto :end
+
+:error
+echo An error occurred during optimization.
 pause
+exit /b 1
+
+:end
+echo Press any key to exit...
+pause >nul
 goto menu
+
+@echo off
+setlocal enabledelayedexpansion
 
 :optimize_internet
-rem Optimize Internet performance
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v TcpNoDelay /t REG_DWORD /d 1 /f
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v TCPDelAckTicks /t REG_DWORD /d 0 /f
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v DefaultTTL /t REG_DWORD /d 64 /f
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v Tcp1323Opts /t REG_DWORD /d 1 /f
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v TcpAckFrequency /t REG_DWORD /d 1 /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" /v NetworkThrottlingIndex /t REG_DWORD /d ffffffff /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" /v SystemResponsiveness /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v AllowTelemetry /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v EnableActivityFeed /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v PublishUserActivities /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v UploadUserActivities /t REG_DWORD /d 0 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SubscribedContent-338389Enabled /t REG_DWORD /d 0 /f
-echo Optimized Internet performance.
+echo Optimizing Internet performance...
+
+REM TCP/IP Optimization
+set "tcp_path=HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters"
+reg add "%tcp_path%" /v "TcpNoDelay" /t REG_DWORD /d 1 /f
+reg add "%tcp_path%" /v "TCPDelAckTicks" /t REG_DWORD /d 0 /f
+reg add "%tcp_path%" /v "DefaultTTL" /t REG_DWORD /d 64 /f
+reg add "%tcp_path%" /v "Tcp1323Opts" /t REG_DWORD /d 1 /f
+reg add "%tcp_path%" /v "TcpAckFrequency" /t REG_DWORD /d 1 /f
+reg add "%tcp_path%" /v "GlobalMaxTcpWindowSize" /t REG_DWORD /d 65535 /f
+reg add "%tcp_path%" /v "TcpWindowSize" /t REG_DWORD /d 65535 /f
+reg add "%tcp_path%" /v "MaxUserPort" /t REG_DWORD /d 65534 /f
+if %errorlevel% neq 0 echo Failed to optimize TCP/IP settings. & goto :error
+
+REM Network Throttling Index
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" /v "NetworkThrottlingIndex" /t REG_DWORD /d 0xffffffff /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" /v "SystemResponsiveness" /t REG_DWORD /d 0 /f
+if %errorlevel% neq 0 echo Failed to set Network Throttling Index. & goto :error
+
+REM Disable Windows Telemetry and Activity Feed
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v "AllowTelemetry" /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "EnableActivityFeed" /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "PublishUserActivities" /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "UploadUserActivities" /t REG_DWORD /d 0 /f
+if %errorlevel% neq 0 echo Failed to disable Windows Telemetry and Activity Feed. & goto :error
+
+REM Disable Content Delivery
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "SubscribedContent-338389Enabled" /t REG_DWORD /d 0 /f
+if %errorlevel% neq 0 echo Failed to disable Content Delivery. & goto :error
+
+REM Clear DNS Cache
+ipconfig /flushdns
+if %errorlevel% neq 0 echo Failed to clear DNS cache. & goto :error
+
+REM Reset Winsock catalog
+netsh winsock reset
+if %errorlevel% neq 0 echo Failed to reset Winsock catalog. & goto :error
+
+REM Disable IPv6 (optional, uncomment if needed)
+REM netsh interface ipv6 set state disabled
+REM if %errorlevel% neq 0 echo Failed to disable IPv6. & goto :error
+
+REM Set DNS servers to Google's public DNS (optional, uncomment if needed)
+REM netsh interface ipv4 set dns name="Ethernet" static 8.8.8.8 primary
+REM netsh interface ipv4 add dns name="Ethernet" 8.8.4.4 index=2
+REM if %errorlevel% neq 0 echo Failed to set DNS servers. & goto :error
+
+echo Internet optimization completed successfully.
+goto :end
+
+:error
+echo An error occurred during optimization.
 pause
-goto menu
+exit /b 1
+
+:end
+echo Press any key to return to menu...
+pause >nul
+goto :menu
 
 :windows_update
-rem Configure Windows Update
+echo Configuring Windows Update...
+
+REM Set Windows Update to automatically download and install updates
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v AUOptions /t REG_DWORD /d 4 /f
+if %errorlevel% neq 0 echo Failed to set automatic update option. & goto :update_error
+
+REM Set updates to install every day
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v ScheduledInstallDay /t REG_DWORD /d 0 /f
+if %errorlevel% neq 0 echo Failed to set update schedule day. & goto :update_error
+
+REM Set updates to install at 3 AM
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v ScheduledInstallTime /t REG_DWORD /d 3 /f
+if %errorlevel% neq 0 echo Failed to set update schedule time. & goto :update_error
+
+REM Include driver updates in Windows Update
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v ExcludeWUDriversInQualityUpdate /t REG_DWORD /d 0 /f
+if %errorlevel% neq 0 echo Failed to include driver updates. & goto :update_error
+
+REM Enable automatic driver updates
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DriverSearching" /v SearchOrderConfig /t REG_DWORD /d 1 /f
-echo Configured Windows Update.
+if %errorlevel% neq 0 echo Failed to enable automatic driver updates. & goto :update_error
+
+REM Disable auto-restart after updates
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v NoAutoRebootWithLoggedOnUsers /t REG_DWORD /d 1 /f
+if %errorlevel% neq 0 echo Failed to disable auto-restart after updates. & goto :update_error
+
+REM Enable automatic update of other Microsoft products
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v AllowMUUpdateService /t REG_DWORD /d 1 /f
+if %errorlevel% neq 0 echo Failed to enable update of other Microsoft products. & goto :update_error
+
+REM Disable delivery optimization
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeliveryOptimization" /v DODownloadMode /t REG_DWORD /d 0 /f
+if %errorlevel% neq 0 echo Failed to disable delivery optimization. & goto :update_error
+
+REM Force Windows Update to immediately check for updates
+echo Checking for Windows updates...
+wuauclt /detectnow
+wuauclt /updatenow
+
+echo Windows Update configuration completed successfully.
+goto :update_end
+
+:update_error
+echo An error occurred during Windows Update configuration.
 pause
-goto menu
+exit /b 1
+
+:update_end
+echo Press any key to return to menu...
+pause >nul
+goto :menu
 
 :auto_login
 set /p username=Enter the username for auto-login: 
@@ -127,22 +248,96 @@ echo Enabled Auto-login.
 pause
 goto menu
 
+@echo off
+setlocal enabledelayedexpansion
+
 :clear_cache
-rem Clear Cache
 echo Clearing system cache...
-del /q /s %TEMP%\*
-del /q /s C:\Windows\Temp\*
-echo Cleared system cache.
+
+REM Clear Windows Temp folder
+del /q /f /s "%WINDIR%\Temp\*.*"
+if %errorlevel% neq 0 echo Failed to clear Windows Temp folder. & goto :cache_error
+
+REM Clear User Temp folder
+del /q /f /s "%TEMP%\*.*"
+if %errorlevel% neq 0 echo Failed to clear User Temp folder. & goto :cache_error
+
+REM Clear Prefetch folder
+del /q /f /s "%WINDIR%\Prefetch\*.*"
+if %errorlevel% neq 0 echo Failed to clear Prefetch folder. & goto :cache_error
+
+REM Clear Windows Update Cache
+net stop wuauserv
+rmdir /s /q C:\Windows\SoftwareDistribution
+mkdir C:\Windows\SoftwareDistribution
+net start wuauserv
+if %errorlevel% neq 0 echo Failed to clear Windows Update Cache. & goto :cache_error
+
+REM Clear DNS Cache
+ipconfig /flushdns
+if %errorlevel% neq 0 echo Failed to clear DNS Cache. & goto :cache_error
+
+REM Clear ARP Cache
+netsh interface ip delete arpcache
+if %errorlevel% neq 0 echo Failed to clear ARP Cache. & goto :cache_error
+
+REM Clear Font Cache
+net stop FontCache
+del /f /s /q "%WinDir%\ServiceProfiles\LocalService\AppData\Local\FontCache\*.*"
+net start FontCache
+if %errorlevel% neq 0 echo Failed to clear Font Cache. & goto :cache_error
+
+echo Cache clearing completed successfully.
+goto :cache_end
+
+:cache_error
+echo An error occurred while clearing cache.
 pause
-goto menu
+exit /b 1
+
+:cache_end
+echo Press any key to return to menu...
+pause >nul
+goto :menu
 
 :optimize_disk
-rem Optimize Disk
 echo Optimizing disk...
-defrag C: /O
-echo Disk optimization complete.
+
+REM Check disk for errors
+echo Checking disk for errors...
+chkdsk C: /f /r /x
+if %errorlevel% neq 0 echo Failed to check disk for errors. & goto :disk_error
+
+REM Clean up system files
+echo Cleaning up system files...
+cleanmgr /sagerun:1
+if %errorlevel% neq 0 echo Failed to clean up system files. & goto :disk_error
+
+REM Optimize drives
+echo Optimizing drives...
+defrag C: /O /U /V
+if %errorlevel% neq 0 echo Failed to optimize drives. & goto :disk_error
+
+REM Disable Hibernation to free up disk space (optional, uncomment if needed)
+REM powercfg /h off
+REM if %errorlevel% neq 0 echo Failed to disable Hibernation. & goto :disk_error
+
+REM Compact OS (only for Windows 10 and later, uncomment if needed)
+REM Compact.exe /CompactOS:always
+REM if %errorlevel% neq 0 echo Failed to compact OS. & goto :disk_error
+
+echo Disk optimization completed successfully.
+goto :disk_end
+
+:disk_error
+echo An error occurred during disk optimization.
 pause
-goto menu
+exit /b 1
+
+:disk_end
+echo Press any key to return to menu...
+pause >nul
+goto :menu
 
 :end
 echo Thank you for using this script!
