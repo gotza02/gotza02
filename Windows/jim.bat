47cff6c17c653e1220216d54432c05e6d0b1b631
gotza02 <47317509+gotza02@users.noreply.github.com>
2024-07-06 20:16:38 +0700

    Update jim.bat

--- Windows/jim.bat
+++ Windows/jim.bat
@@ -1,1 +1,83 @@
+@echo off
+echo Windows Update Fix Script
+echo.
+echo This script will attempt to fix Windows Update issues.
+echo Please run this script as an administrator.
+echo.
+pause
 
+echo Stopping Windows Update related services...
+net stop wuauserv
+net stop cryptSvc
+net stop bits
+net stop msiserver
+
+echo Renaming SoftwareDistribution and Catroot2 folders...
+ren C:\Windows\SoftwareDistribution SoftwareDistribution.old
+ren C:\Windows\System32\catroot2 catroot2.old
+
+echo Resetting Windows Update components...
+sc sdset bits D:(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWLOCRRC;;;AU)(A;;CCLCSWRPWPDTLOCRRC;;;PU)
+sc sdset wuauserv D:(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWLOCRRC;;;AU)(A;;CCLCSWRPWPDTLOCRRC;;;PU)
+
+echo Registering Windows Update related DLLs...
+regsvr32.exe /s atl.dll
+regsvr32.exe /s urlmon.dll
+regsvr32.exe /s mshtml.dll
+regsvr32.exe /s shdocvw.dll
+regsvr32.exe /s browseui.dll
+regsvr32.exe /s jscript.dll
+regsvr32.exe /s vbscript.dll
+regsvr32.exe /s scrrun.dll
+regsvr32.exe /s msxml.dll
+regsvr32.exe /s msxml3.dll
+regsvr32.exe /s msxml6.dll
+regsvr32.exe /s actxprxy.dll
+regsvr32.exe /s softpub.dll
+regsvr32.exe /s wintrust.dll
+regsvr32.exe /s dssenh.dll
+regsvr32.exe /s rsaenh.dll
+regsvr32.exe /s gpkcsp.dll
+regsvr32.exe /s sccbase.dll
+regsvr32.exe /s slbcsp.dll
+regsvr32.exe /s cryptdlg.dll
+regsvr32.exe /s oleaut32.dll
+regsvr32.exe /s ole32.dll
+regsvr32.exe /s shell32.dll
+regsvr32.exe /s initpki.dll
+regsvr32.exe /s wuapi.dll
+regsvr32.exe /s wuaueng.dll
+regsvr32.exe /s wuaueng1.dll
+regsvr32.exe /s wucltui.dll
+regsvr32.exe /s wups.dll
+regsvr32.exe /s wups2.dll
+regsvr32.exe /s wuweb.dll
+regsvr32.exe /s qmgr.dll
+regsvr32.exe /s qmgrprxy.dll
+regsvr32.exe /s wucltux.dll
+regsvr32.exe /s muweb.dll
+regsvr32.exe /s wuwebv.dll
+
+echo Resetting Winsock and WinHTTP proxy...
+netsh winsock reset
+netsh winhttp reset proxy
+
+echo Resetting Windows Update policies...
+gpupdate /force
+
+echo Starting Windows Update related services...
+net start bits
+net start wuauserv
+net start appidsvc
+net start cryptSvc
+
+echo Clearing Windows Update cache...
+del /s /q /f "%ALLUSERSPROFILE%\Application Data\Microsoft\Network\Downloader\qmgr*.dat"
+del /s /q /f "%ALLUSERSPROFILE%\Microsoft\Network\Downloader\qmgr*.dat"
+
+echo Running Windows Update troubleshooter...
+msdt.exe /id WindowsUpdateDiagnostic
+
+echo Script execution completed.
+echo Please restart your computer and try running Windows Update again.
+pause
